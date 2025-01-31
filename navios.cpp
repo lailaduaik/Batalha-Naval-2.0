236931df454da45b1a2fddb4e549ac7327cc4044
Laila <lailarduaik@gmail.com>
2025-01-31 08:17:04 -0300

    Update navios.cpp
    documentação

--- navios.cpp
+++ navios.cpp
@@ -1,55 +1,114 @@
-
-
 #include "..\include\navios.hpp"
+#include <iostream>
 
+using namespace std;
 
-//construtor padrão da classe navios, que recebe a altura, largura, tipo e quantidade por tipo, como a direção é para cada navio foi preferível alterar depois.
-Navios :: Navios(int _altura, int _largura, char _tipo, int _quantidade):altura(_altura), largura(_largura), tipo(_tipo), quantidade(_quantidade){} 
+/**
+ * @file navios.cpp
+ * @brief Implementação das classes que representam os navios do jogo.
+ */
 
-//pega a altura e retorna ela
-int Navios :: get_altura() const{
+/**
+ * @brief Construtor da classe Navios.
+ *
+ * Inicializa um navio com altura, largura, tipo e quantidade.
+ * A direção (`comando_direcao`) deve ser definida posteriormente.
+ *
+ * @param _altura Altura do navio.
+ * @param _largura Largura do navio.
+ * @param _tipo Tipo do navio ('C' para Carrier, 'T' para Tanker, etc.).
+ * @param _quantidade Quantidade de navios desse tipo disponíveis.
+ */
+Navios::Navios(int _altura, int _largura, char _tipo, int _quantidade)
+    : altura(_altura), largura(_largura), tipo(_tipo), quantidade(_quantidade) {}
+
+/**
+ * @brief Obtém a altura do navio.
+ * 
+ * @return A altura do navio.
+ */
+int Navios::get_altura() const {
     return altura;
 }
 
-// pega a largura e retorna ela
-int Navios :: get_largura() const{
+/**
+ * @brief Obtém a largura do navio.
+ * 
+ * @return A largura do navio.
+ */
+int Navios::get_largura() const {
     return largura;
 }
 
-//pega o tipo e retorna ele
-char Navios :: get_tipo() const{ 
+/**
+ * @brief Obtém o tipo do navio.
+ * 
+ * @return O caractere representando o tipo do navio.
+ */
+char Navios::get_tipo() const {
     return tipo;
 }
 
-//pega a quantidade e retorna ele
-int Navios :: getquantidade() const{
+/**
+ * @brief Obtém a quantidade de navios disponíveis desse tipo.
+ * 
+ * @return O número de navios disponíveis.
+ */
+int Navios::getquantidade() const {
     return quantidade;
 }
 
-// seta a direção do navio
-void Navios :: setdirecao(char _direcao){
+/**
+ * @brief Define a direção do navio ('H' para horizontal, 'V' para vertical).
+ * 
+ * @param _direcao Direção do navio.
+ */
+void Navios::setdirecao(char _direcao) {
     comando_direcao = _direcao;
 }
 
-// devolve a direção do navio H ou V
-char Navios :: getdirecao() const{
+/**
+ * @brief Obtém a direção do navio ('H' para horizontal, 'V' para vertical).
+ * 
+ * @return O caractere representando a direção do navio.
+ */
+char Navios::getdirecao() const {
     return comando_direcao;
 }
 
-//set a quantidade
-void Navios ::setquantidade(int _quantidade){
+/**
+ * @brief Define a quantidade de navios disponíveis desse tipo.
+ * 
+ * @param _quantidade Novo valor para a quantidade de navios disponíveis.
+ */
+void Navios::setquantidade(int _quantidade) {
     quantidade = _quantidade;
 }
 
-//inicializa 1 carrier
-Carrier::Carrier():Navios(4,1,'C',1) {}
+/**
+ * @brief Construtor da classe Carrier.
+ *
+ * Inicializa um porta-aviões com altura 4, largura 1 e quantidade 1.
+ */
+Carrier::Carrier() : Navios(4, 1, 'C', 1) {}
 
-//inicializa os 2 tankers
-Tanker::Tanker(): Navios(3,1,'T',2){}
+/**
+ * @brief Construtor da classe Tanker.
+ *
+ * Inicializa dois navios do tipo Tanker, cada um com altura 3 e largura 1.
+ */
+Tanker::Tanker() : Navios(3, 1, 'T', 2) {}
 
-//inicializa os 2 destroyers
-Destroyer::Destroyer(): Navios(3,2,'D', 2){}
+/**
+ * @brief Construtor da classe Destroyer.
+ *
+ * Inicializa dois navios do tipo Destroyer, cada um com altura 3 e largura 2.
+ */
+Destroyer::Destroyer() : Navios(3, 2, 'D', 2) {}
 
-//inicializa os 5 submarinos
-Submarine::Submarine():  Navios(1,1,'S', 5){}
-
+/**
+ * @brief Construtor da classe Submarine.
+ *
+ * Inicializa cinco submarinos, cada um com altura 1 e largura 1.
+ */
+Submarine::Submarine() : Navios(1, 1, 'S', 5) {}
